ae9e395a33530cef13679380f5fb84e8c59c9c07
Francesco Battaglini <francesco@battaglini.net>
2026-01-07 12:27:46 +1100

    feat: refactored uri creation

--- src/main/kotlin/net/battaglini/ptvmcpserver/client/PtvClient.kt
+++ src/main/kotlin/net/battaglini/ptvmcpserver/client/PtvClient.kt
@@ -1,5 +1,10 @@
 package net.battaglini.ptvmcpserver.client
 
+import jakarta.annotation.PostConstruct
+import net.battaglini.ptvmcpserver.configuration.CacheConfiguration.Companion.PTV_DISRUPTIONS_CACHE_NAME
+import net.battaglini.ptvmcpserver.configuration.CacheConfiguration.Companion.PTV_ROUTES_CACHE_NAME
+import net.battaglini.ptvmcpserver.configuration.CacheConfiguration.Companion.PTV_ROUTE_CACHE_NAME
+import net.battaglini.ptvmcpserver.configuration.CacheConfiguration.Companion.PTV_STOPS_CACHE_NAME
 import net.battaglini.ptvmcpserver.configuration.PtvClientProperties
 import net.battaglini.ptvmcpserver.dto.PtvDisruptionsResponseDto
 import net.battaglini.ptvmcpserver.dto.PtvRouteResponseDto
@@ -10,6 +15,8 @@ import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.awaitBody
+import org.springframework.web.util.DefaultUriBuilderFactory
+import org.springframework.web.util.UriBuilderFactory
 import java.net.URI
 import java.util.*
 import javax.crypto.Mac
@@ -20,92 +27,91 @@ class PtvClient(
     private val ptvClientProperties: PtvClientProperties,
     private val ptvWebClient: WebClient,
 ) {
-    @Cacheable("ptvRoutes")
+    private lateinit var uriBuilderFactory: UriBuilderFactory
+
+    @PostConstruct
+    fun init() {
+        uriBuilderFactory = DefaultUriBuilderFactory(ptvClientProperties.baseUrl)
+    }
+
+    @Cacheable(PTV_ROUTES_CACHE_NAME)
     suspend fun getRoutes(): PtvRoutesResponseDto {
         LOGGER.debug("Getting all PTV routes...")
+
+        val uri = buildRequestURI("/{apiVersion}/routes", ptvClientProperties.apiVersion)
+
         return ptvWebClient.get()
-            .uri { uriBuilder ->
-                val uri = uriBuilder.path("/${ptvClientProperties.apiVersion}/routes")
-                    .queryParam(DEVID_QUERY_PARAM_KEY, ptvClientProperties.username)
-                    .build()
-                val signature = calculateHMACSignature(uri)
-                return@uri uriBuilder
-                    .queryParam(SIGNATURE_QUERY_PARAM_KEY, signature)
-                    .build()
-            }
+            .uri(uri)
             .retrieve()
             .awaitBody<PtvRoutesResponseDto>()
     }
 
-    @Cacheable("ptvRoute")
+    @Cacheable(PTV_ROUTE_CACHE_NAME)
     suspend fun getRoute(routeId: Int): PtvRouteResponseDto {
         LOGGER.debug("Getting info for route $routeId...")
+
+        val uri = buildRequestURI("/{apiVersion}/routes/{routeId}", ptvClientProperties.apiVersion, routeId)
+
         return ptvWebClient.get()
-            .uri { uriBuilder ->
-                val uri = uriBuilder.path("/${ptvClientProperties.apiVersion}/routes/{routeId}")
-                    .queryParam(DEVID_QUERY_PARAM_KEY, ptvClientProperties.username)
-                    .build(routeId)
-                val signature = calculateHMACSignature(uri)
-                return@uri uriBuilder
-                    .queryParam(SIGNATURE_QUERY_PARAM_KEY, signature)
-                    .build(routeId)
-            }
+            .uri(uri)
             .retrieve()
             .awaitBody<PtvRouteResponseDto>()
     }
 
-    @Cacheable("ptvStops")
+    @Cacheable(PTV_STOPS_CACHE_NAME)
     suspend fun getStops(routeId: Int, routeType: Int): PtvRouteStopsResponseDto {
         LOGGER.debug("Getting all stops for route=$routeId, routeType=$routeType...")
+
+        val uri = buildRequestURI(
+            "/{apiVersion}/stops/route/{routeId}/route_type/{routeType}",
+            ptvClientProperties.apiVersion,
+            routeId,
+            routeType
+        )
+
         return ptvWebClient.get()
-            .uri { uriBuilder ->
-                val uri =
-                    uriBuilder.path("/${ptvClientProperties.apiVersion}/stops/route/{routeId}/route_type/{routeType}")
-                        .queryParam(DEVID_QUERY_PARAM_KEY, ptvClientProperties.username)
-                        .build(routeId, routeType)
-                val signature = calculateHMACSignature(uri)
-                return@uri uriBuilder
-                    .queryParam(SIGNATURE_QUERY_PARAM_KEY, signature)
-                    .build(routeId, routeType)
-            }
+            .uri(uri)
             .retrieve()
             .awaitBody<PtvRouteStopsResponseDto>()
     }
 
-    @Cacheable("ptvRouteDisruptions")
+    @Cacheable(PTV_DISRUPTIONS_CACHE_NAME)
     suspend fun getRouteDisruptions(routeId: Int, stopId: Int?): PtvDisruptionsResponseDto {
         LOGGER.debug("Getting disruptions for route=$routeId, stopId=$stopId...")
+
+        val uri = stopId?.let {
+            buildRequestURI(
+                "/{apiVersion}/disruptions/route/{route_id}/stop/{stop_id}",
+                ptvClientProperties.apiVersion,
+                routeId,
+                stopId
+            )
+        } ?: buildRequestURI("/{apiVersion}/disruptions/route/{route_id}", ptvClientProperties.apiVersion, routeId)
+
         return ptvWebClient.get()
-            .uri { uriBuilder ->
-                var uri: URI;
-                if (stopId != null) {
-                    uri =
-                        uriBuilder.path("/${ptvClientProperties.apiVersion}/disruptions/route/{route_id}/stop/{stop_id}")
-                            .queryParam(DEVID_QUERY_PARAM_KEY, ptvClientProperties.username)
-                            .build(routeId, stopId)
-                } else {
-                    uri = uriBuilder.path("/${ptvClientProperties.apiVersion}/disruptions/route/{route_id}")
-                        .queryParam(DEVID_QUERY_PARAM_KEY, ptvClientProperties.username)
-                        .build(routeId)
-                }
-                val signature = calculateHMACSignature(uri)
-                uriBuilder
-                    .queryParam(SIGNATURE_QUERY_PARAM_KEY, signature)
-                if (stopId != null) {
-                    return@uri uriBuilder
-                        .build(routeId, stopId)
-                }
-                return@uri uriBuilder
-                    .build(routeId)
-            }
+            .uri(uri)
             .retrieve()
             .awaitBody<PtvDisruptionsResponseDto>()
     }
 
+    internal fun buildRequestURI(path: String, vararg uriVariables: Any): URI {
+        val builder = uriBuilderFactory.builder()
+
+        val tempUri = builder.path(path)
+            .queryParam(DEVID_QUERY_PARAM_KEY, ptvClientProperties.username)
+            .build(uriVariables)
+
+        val signature = calculateHMACSignature(tempUri)
+
+        return builder
+            .queryParam(SIGNATURE_QUERY_PARAM_KEY, signature)
+            .build(uriVariables)
+    }
+
     internal fun calculateHMACSignature(uri: URI): String {
         // Remove the baseUrl from the string
-        val uriBytes = uri.toString().replace(ptvClientProperties.baseUrl, "/").byteInputStream().readBytes();
-        val keyBytes = ptvClientProperties.password.byteInputStream().readBytes();
+        val uriBytes = uri.toString().replace(ptvClientProperties.baseUrl, "/").byteInputStream().readBytes()
+        val keyBytes = ptvClientProperties.password.byteInputStream().readBytes()
 
         val signingKey = SecretKeySpec(keyBytes, HMAC_SHA1_ALGORITHM)
         val mac = Mac.getInstance(HMAC_SHA1_ALGORITHM)
